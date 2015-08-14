fb10ae051e703edb9bfa021b83a20354de620a20
tunnckoCore <mameto_100@mail.bg>
2015-08-15 01:54:29 +0300

    move utils to ./lib

--- index.js
+++ index.js
@@ -11,13 +11,9 @@
 
 var util = require('util')
 var Benz = require('benz')
-var merge = require('merge-deep')
-var isObject = require('is-extendable')
+var utils = require('./lib/utils')
 var manageArguments = require('manage-arguments')
 var handleArguments = require('handle-arguments')
-var is = require('is-kindof')
-
-module.exports = Vez
 
 function Vez (options) {
   if (!(this instanceof Vez)) {
@@ -31,23 +27,23 @@ function Vez (options) {
 util.inherits(Vez, Benz)
 
 Vez.prototype.use = function use () {
-  this._fns = compose(this._fns, manageArguments(arguments))
+  this._fns = utils.compose(this._fns, manageArguments(arguments))
   return this
 }
 
 Vez.prototype.series = function series () {
-  factory('series', this, arguments)
+  utils.factory('series', this, arguments)
   return this
 }
 
 Vez.prototype.parallel = function parallel () {
-  factory('parallel', this, arguments)
+  utils.factory('parallel', this, arguments)
   return this
 }
 
 Vez.prototype.run = function run () {
   var argz = handleArguments(arguments)
-  setContext(this, argz.args)
+  utils.setContext(this, argz.args)
 
   if (this.enabled('parallel')) {
     var parallel = this.compose('parallel')
@@ -58,34 +54,4 @@ Vez.prototype.run = function run () {
   series(this._fns, this.option('extensions'))(argz.callback)
 }
 
-function factory (method, self, argsObject) {
-  var argz = handleArguments(argsObject)
-  setContext(this, argz.args)
-
-  var flow = self.compose(method)
-  var done = flow(self._fns, self.option('extensions'))
-  done(argz.callback)
-}
-
-function setContext (self, args) {
-  var contexts = args.filter(isObject)
-  var ctx = merge.apply(null, [{}, self.option('context')].concat(contexts))
-  self.option('context', ctx)
-}
-
-function compose (fns, middlewares) {
-  var len = middlewares.length
-  var i = 0
-
-  while (i < len) {
-    var mw = middlewares[i++]
-    if (!allowedMiddleware(mw)) continue
-    fns.push(mw)
-  }
-
-  return fns
-}
-
-function allowedMiddleware (mw) {
-  return is(mw, 'promise') || is(mw, 'generatorfunction') || is(mw, 'function')
-}
+module.exports = Vez
